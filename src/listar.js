190ca5dfd58e8fca295f8fada430f09cfbd01458
Guilherme Stollmeier <guilhermestollmeier@guilhermes-mbp.local>
2018-02-27 10:49:43 -0300

    delete import

--- src/listar.js
+++ src/listar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router-dom';
 import Button from 'muicss/lib/react/button';
